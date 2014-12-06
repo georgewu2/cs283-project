6177b027e348983ce6f88cbcbfcf75d6edd90e68
George Wu <georgewu@college.harvard.edu>
2014-12-06 10:33:19 -0500

    added demo 2, mixed seamless cloning implemented

--- src/demo2.m
+++ src/demo2.m
@@ -2,21 +2,22 @@
 % Demo 1
 % Demonstrates seamless cloning
 
-sourceIm = imread('../data/water_front.png');
-destIm = imread('../data/water_back.jpeg');
+sourceIm = imread('../data/cat.jpg');
+destIm = imread('../data/hawaii.jpg');
 imMask = zeros(size(sourceIm,1),size(sourceIm,2));
-y1 = 21;
-y2 = 80;
-x1 = 44;
-x2 = 153;
+y1 = 285;
+y2 = 500;
+x1 = 150;
+x2 = 550;
 imMask(y1:y2,x1:x2) = 1;
-offsetY = 0;
+offsetY = 370;
 offsetX = 0;
 
 pastedIm = destIm;
 pastedIm(y1:y2,x1:x2,:) = sourceIm(y1:y2,x1:x2,:);
 
 clonedIm = SeamlessClone(destIm,sourceIm,imMask,offsetY,offsetX);
+mixedClonedIm = MixedSeamlessClone(destIm,sourceIm,imMask,offsetY,offsetX);
 
 figure;
 subplot(2,2,1);
@@ -26,8 +27,11 @@ subplot(2,2,2);
 imshow(sourceIm);
 title('Source Image');
 subplot(2,2,3);
-imshow(pastedIm);
-title('Direct pasting');
-subplot(2,2,4);
 imshow(clonedIm);
 title('Poisson seamless cloning');
+subplot(2,2,4);
+imshow(mixedClonedIm);
+title('Mixed Poisson Seamless Cloning');
+
+print -djpeg '../data/results/demo2';
+
